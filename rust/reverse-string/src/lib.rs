9994dbd612104b24c228f4cb9f4d918c339a72e4
Raphael Uziel <raphaeluziel@hotmail.com>
2025-01-05 16:46:32 -0500

    on time

--- rust/reverse-string/src/lib.rs
+++ rust/reverse-string/src/lib.rs
@@ -1,3 +1,16 @@
+use unicode_reverse::reverse_grapheme_clusters_in_place;
+
 pub fn reverse(input: &str) -> String {
-    todo!("Write a function to reverse {input}");
+    // todo!("Write a function to reverse {input}");
+
+    // let s = input.chars().rev();
+    // let mut r = String::new();
+    // for c in s {
+    //     r.push(c);
+    // }
+    // r
+
+    let mut s = input.to_string();
+    reverse_grapheme_clusters_in_place(&mut s);
+    s
 }
