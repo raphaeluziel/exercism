19a778d3efc28c1982168db2723565be13e0b824
Raphael Uziel <raphaeluziel@hotmail.com>
2025-01-07 16:39:01 -0500

    start clock

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -1,6 +1,29 @@
-use time::PrimitiveDateTime as DateTime;
+use time::{Duration, PrimitiveDateTime as DateTime};
+
+const GIGASEC: time::Duration = Duration::new(1_000_000_000, 0);
 
 // Returns a DateTime one billion seconds after start.
 pub fn after(start: DateTime) -> DateTime {
-    todo!("What time is a gigasecond later than {start}");
+    // todo!("What time is a gigasecond later than {start}");
+
+    // Method 1
+    // start.checked_add(GIGASEC).unwrap()
+
+    // Method 2
+    // match start.checked_add(GIGASEC) {
+    //     Some(x) => x,
+    //     None => start
+    // }
+
+    // Method 3
+    // if let Some(x) = start.checked_add(GIGASEC) {
+    //     x
+    // }
+    // else {
+    //     start
+    // }
+
+    // Method 4
+    // Best, I think
+    start.checked_add(GIGASEC).unwrap_or(start)
 }
