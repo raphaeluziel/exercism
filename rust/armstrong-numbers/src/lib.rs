73257b29997ac6e93e792dbcd4d6f613ab8fd761
raphaeluziel <raphaeluziel@gmail.com>
2025-01-11 19:09:59 -0500

    armstrong

--- rust/armstrong-numbers/src/lib.rs
+++ rust/armstrong-numbers/src/lib.rs
@@ -1,3 +1,19 @@
 pub fn is_armstrong_number(num: u32) -> bool {
-    todo!("true if {num} is an armstrong number")
+    let mut x:u32 = num;
+    let divisor = 10;
+    let mut digits = Vec::new();
+
+    while x != 0 {
+        digits.push(x % divisor);
+         x /= 10;
+    }
+
+    let mut armstrong_number = 0;
+    let num_digits = digits.len();
+
+    for d in &digits {
+        armstrong_number += d.pow(num_digits as u32);
+    }
+    
+    armstrong_number == num
 }
