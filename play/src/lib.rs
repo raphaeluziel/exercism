8ba184a1634024a446926988c0376b4d9d3b4192
raphaeluziel <raphaeluziel@gmail.com>
2025-01-21 18:57:52 -0500

    what?

--- play/src/lib.rs
+++ play/src/lib.rs
@@ -7,8 +7,6 @@ pub enum Comparison {
 }
 
 pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Comparison {
-    let a = [1, 2, 3, 9 ];
-    let b = [990, 880, 1, 2, 3, 9, 110, 220, 330, 440];
 
     let sm: &[T] = if _first_list.len() <= _second_list.len() { &_first_list } else { &_second_list };
     let lg: &[T] = if _first_list.len() > _second_list.len() { &_first_list } else { &_second_list };
