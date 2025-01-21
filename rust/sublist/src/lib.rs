025bb66ff492c77eda5e9d2418d58e68c1876151
raphaeluziel <raphaeluziel@gmail.com>
2025-01-21 18:51:55 -0500

    use cmp

--- rust/sublist/src/lib.rs
+++ rust/sublist/src/lib.rs
@@ -7,11 +7,16 @@ pub enum Comparison {
 }
 
 pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Comparison {
-    let a = [1, 2, 3, 9 ];
-    let b = [990, 880, 1, 2, 3, 9, 110, 220, 330, 440];
-
-    let sm: &[T] = if _first_list.len() <= _second_list.len() { &_first_list } else { &_second_list };
-    let lg: &[T] = if _first_list.len() > _second_list.len() { &_first_list } else { &_second_list };
+    let sm: &[T] = if _first_list.len() <= _second_list.len() {
+        _first_list
+    } else {
+        _second_list
+    };
+    let lg: &[T] = if _first_list.len() > _second_list.len() {
+        _first_list
+    } else {
+        _second_list
+    };
 
     if sm == lg {
         return Comparison::Equal;
@@ -20,9 +25,13 @@ pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Compariso
     let smi = sm.len();
     let lgi = lg.len();
 
-    let cp = if _first_list.len() > _second_list.len() { Comparison::Superlist } 
-    else if _first_list.len() < _second_list.len() { Comparison::Sublist }
-    else { Comparison::Unequal };
+    let cp = if _first_list.len() > _second_list.len() {
+        Comparison::Superlist
+    } else if _first_list.len() < _second_list.len() {
+        Comparison::Sublist
+    } else {
+        Comparison::Unequal
+    };
 
     for i in 0..=(lgi - smi) {
         let lg_slice: &[T] = &lg[i..(i + smi)];
