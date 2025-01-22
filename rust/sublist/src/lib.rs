b9f6efc0af0ecabae1c3115f900bf3b695e7e7d8
Raphael Uziel <raphaeluziel@hotmail.com>
2025-01-22 16:45:40 -0500

    second sub

--- rust/sublist/src/lib.rs
+++ rust/sublist/src/lib.rs
@@ -1,3 +1,5 @@
+use std::cmp::Ordering;
+
 #[derive(Debug, PartialEq, Eq)]
 pub enum Comparison {
     Equal,
@@ -7,37 +9,23 @@ pub enum Comparison {
 }
 
 pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Comparison {
-    let sm: &[T] = if _first_list.len() <= _second_list.len() {
-        _first_list
-    } else {
-        _second_list
-    };
-    let lg: &[T] = if _first_list.len() > _second_list.len() {
-        _first_list
-    } else {
-        _second_list
-    };
+    let sm: &[T] = if _first_list.len() <= _second_list.len() { _first_list } else { _second_list };
+    let lg: &[T] = if _first_list.len()  > _second_list.len() { _first_list } else { _second_list };
 
-    if sm == lg {
-        return Comparison::Equal;
-    }
+    if sm == lg { return Comparison::Equal; }
 
     let smi = sm.len();
     let lgi = lg.len();
 
-    let cp = if _first_list.len() > _second_list.len() {
-        Comparison::Superlist
-    } else if _first_list.len() < _second_list.len() {
-        Comparison::Sublist
-    } else {
-        Comparison::Unequal
+    let cp = match _first_list.len().cmp(&_second_list.len()) {
+        Ordering::Greater => Comparison::Superlist,
+        Ordering::Less => Comparison::Sublist,
+        Ordering::Equal => Comparison::Unequal
     };
 
     for i in 0..=(lgi - smi) {
         let lg_slice: &[T] = &lg[i..(i + smi)];
-        if sm == lg_slice {
-            return cp;
-        }
+        if sm == lg_slice { return cp; }
     }
 
     Comparison::Unequal
---
+++ rust/sublist/src/lib1.rs
@@ -0,0 +1,44 @@
+#[derive(Debug, PartialEq, Eq)]
+pub enum Comparison {
+    Equal,
+    Sublist,
+    Superlist,
+    Unequal,
+}
+
+pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Comparison {
+    let sm: &[T] = if _first_list.len() <= _second_list.len() {
+        _first_list
+    } else {
+        _second_list
+    };
+    let lg: &[T] = if _first_list.len() > _second_list.len() {
+        _first_list
+    } else {
+        _second_list
+    };
+
+    if sm == lg {
+        return Comparison::Equal;
+    }
+
+    let smi = sm.len();
+    let lgi = lg.len();
+
+    let cp = if _first_list.len() > _second_list.len() {
+        Comparison::Superlist
+    } else if _first_list.len() < _second_list.len() {
+        Comparison::Sublist
+    } else {
+        Comparison::Unequal
+    };
+
+    for i in 0..=(lgi - smi) {
+        let lg_slice: &[T] = &lg[i..(i + smi)];
+        if sm == lg_slice {
+            return cp;
+        }
+    }
+
+    Comparison::Unequal
+}
