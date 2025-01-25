649f1fa7ce1ffc58d5bc0c55f2e829db8a4d649a
Raphael Uziel <raphaeluziel@hotmail.com>
2025-01-25 15:54:36 -0500

    nothing

--- play/src/lib.rs
+++ play/src/lib.rs
@@ -1,19 +1,25 @@
 pub fn annotate(minefield: &[&str]) -> Vec<String> {
 
+    let vs: Vec<String> = Vec::new();
     let m = minefield[0].as_bytes();
+    let stlen = minefield[0].len();
+    println!("STLEN {:?}", stlen);
+
+
+    println!("M -------------------------------> {:?}", m);
 
     println!("MINEFIELD\n{:?}", minefield);
 
 
     for (r, row) in minefield.iter().enumerate() {
         println!();
-        for (c, col) in row.as_bytes().iter().enumerate() {
-            println!("(R_{}, C_{}, VAL__{})", r, c, col);
+        // for (c, col) in row.as_bytes().iter().enumerate() {
+        //     println!("(R_{}, C_{}, VAL__{})", r, c, col);
+        // }
 
-            
+        
 
-        }
     }
 
-    todo!("\nAnnotate each square of the given minefield with the number of mines that surround said square (blank if there are no surrounding mines):\n{minefield:?}\n");
+    vs
 }
