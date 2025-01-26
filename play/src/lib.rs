4693fb3ea4e69f34e67001d6d5c1e0b8b2984d7b
raphaeluziel <raphaeluziel@gmail.com>
2025-01-26 18:20:50 -0500

    crazy!

--- play/src/lib.rs
+++ play/src/lib.rs
@@ -1,17 +1,22 @@
-use std::io::empty;
-
 pub fn annotate(minefield: &[&str]) -> Vec<String> {
     let minemap: Vec<String> = Vec::new();
 
     let rows = minefield.len();
     let cols = minefield[0].len();
 
+    let mut vvv = vec![vec![0u8; cols + 2]; rows + 2];
+    
     for r in 0..rows {
         let row = minefield[r].as_bytes();
+
         for c in 0..cols {
-            println!("HEY {:?}", row.get(c));
+            if row[c] == 42u8 { vvv[r + 1][c + 1] += 1; }
         }
+
+
     }
+
+    println!("{:?}", vvv);
 
     minemap
 }
