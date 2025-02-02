1676b03b26e711df75232bb3a705503d0752a9ad
Raphael Uziel <raphaeluziel@hotmail.com>
2025-02-02 16:21:06 -0500

    tired again

--- play/src/main.rs
+++ play/src/main.rs
@@ -1,6 +1,7 @@
 use play::*;
 
 fn main() {
-    let x = "HHH059";
-    println!("{}  {}", x, is_valid(x));
+    is_valid("hell");
+    // let x = "HHH059";
+    // println!("{}  {}", x, is_valid(x));
 }
