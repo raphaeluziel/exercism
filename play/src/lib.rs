509ed85dc16b2d661cd7a0b79313bedbd91b0ff4
raphaeluziel <raphaeluziel@gmail.com>
2025-02-22 11:15:25 -0500

    working

--- play/src/lib.rs
+++ play/src/lib.rs
@@ -1,38 +1,23 @@
-use std::mem::take;
-
 pub fn recite(start_bottles: u32, take_down: u32) -> String {
-    let mut song = String::new();
-    let bottles = ["bottle", "bottles"];
-
-    let num = [
-        "Ten", 
-        "Nine", 
-        "Eight", 
-        "Seven", 
-        "Six", 
-        "Five", 
-        "Four", 
-        "Three", 
-        "Two", 
-        "One", 
-        "Zero"
-        ];
+    let num = ["Zero", "One", "Two", "Three", "Fouir", "Five", "Six", "Seven", "Eight", "Nine", "Ten"];
 
     let last_bottle = start_bottles - take_down;
     println!("Start     {start_bottles}\nTakedown  {take_down}\nLast      {last_bottle}");
 
-    let x = format!("{} green {} hanging on the wall,\n", start_bottles, bottles[1]);
-    for _ in 0..2 { song.push_str(&x); }
-    song.push_str("And if one green bottle should happen to fall,\n");
-    song.push_str();
+    let i = 2;
 
+    let s1 = if i > 1 { "s" } else { "" };
+    let s2 = if i > 2 { "s" } else { "" };
 
-    println!("SONG\n{}", song);
-
-    // Four green bottles hanging on the wall,
-    // Four green bottles hanging on the wall,
-    // And if one green bottle should accidentally fall,
-    // There'll be three green bottles hanging on the wall.
+    let song = format!("{0} green bottle{1} hanging on the wall,\n\
+                        {0} green bottle{1} hanging on the wall,\n\
+                        And if one green bottle should accidentally fall,\n\
+                        There'll be {2} green bottle{3} hanging on the wall.\n\n", 
+                        num[start_bottles as usize], s1,
+                        num[start_bottles as usize - 1].to_ascii_lowercase(), s2
+                      );
+                          
+    println!("SONG\n\n{song}--------------------------");
 
     song
 }
