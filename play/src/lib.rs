4365e0a33a0fcff8125045f3f01ac88c2bce275e
Raphael Uziel <raphaeluziel@hotmail.com>
2025-03-05 17:08:21 -0500

    bad logic

--- play/src/lib.rs
+++ play/src/lib.rs
@@ -1,9 +1,33 @@
 pub fn abbreviate(phrase: &str) -> String {
     let mut acronym = String::new();
     let phrase = "Something - I made up from thin air";
-    for c in phrase.chars() {
-        if c.is_uppercase() { acronym.push(c); }
+
+    let letters = phrase.char_indices();
+
+    for letter in letters {
+        let mut white_before = false;
+        let mut lower_before = false;
+        println!("Letter = {}", letter.1);
+        if letter.1 == ' ' || letter.1 == '-' {
+            println!("whitespace");
+            white_before = true;
+        } else if letter.1.is_lowercase() {
+            println!("lowercase");
+            lower_before = true;
+            if white_before {
+                println!("white before");
+                acronym.push(letter.1);
+            }
+        }
+        else if letter.1.is_uppercase() {
+            println!("uppercase");
+            if white_before || lower_before {
+                acronym.push(letter.1);
+            }
+        }
     }
-    println!("Acronym = {acronym}");
+
+    println!("Acronym = {:?}", acronym);
+
     acronym
 }
