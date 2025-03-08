7a75aa54824bf8b588056685742077423aa85d69
Raphael Uziel <raphaeluziel@hotmail.com>
2025-03-08 15:31:35 -0500

    just started all-your-base

--- play/src/lib.rs
+++ play/src/lib.rs
@@ -1,3 +1,5 @@
+use core::num;
+
 #[derive(Debug, PartialEq, Eq)]
 pub enum Error {
     InvalidInputBase,
@@ -5,36 +7,29 @@ pub enum Error {
     InvalidDigit(u32),
 }
 
-///
-/// Convert a number between two bases.
-///
-/// A number is any slice of digits.
-/// A digit is any unsigned integer (e.g. u8, u16, u32, u64, or usize).
-/// Bases are specified as unsigned integers.
-///
-/// Return the corresponding Error enum if the conversion is impossible.
-///
-///
-/// You are allowed to change the function signature as long as all test still pass.
-///
-///
-/// Example:
-/// Input
-///   number: &[4, 2]
-///   from_base: 10
-///   to_base: 2
-/// Result
-///   Ok(vec![1, 0, 1, 0, 1, 0])
-///
-/// The example corresponds to converting the number 42 from decimal
-/// which is equivalent to 101010 in binary.
-///
-///
-/// Notes:
-///  * The empty slice ( "[]" ) is equal to the number 0.
-///  * Never output leading 0 digits, unless the input number is 0, in which the output must be `[0]`.
-///    However, your function must be able to process input with leading 0 digits.
-///
 pub fn convert(number: &[u32], from_base: u32, to_base: u32) -> Result<Vec<u32>, Error> {
+    let number = &[2, 10];
+    let mut numvec = number.to_vec();
+    numvec.reverse();
+    let from_base = 16u32;
+    let to_base = 3u32;
+    let output_digits = vec![1, 1, 2, 0];
+
+    let decimal = 0;
+
+    for digit in numvec {
+        println!("digit = {:?}", digit);
+    }
+
+    println!("Deciaml = {decimal}");
+
+    println!("Number = {}", number[0]*from_base.pow(1) 
+                          + number[1]*from_base.pow(0));
+
+    println!("Output = {}", output_digits[0]*to_base.pow(3) 
+                          + output_digits[1]*to_base.pow(2) 
+                          + output_digits[2]*to_base.pow(1) 
+                          + output_digits[3]*to_base.pow(0));
+
     todo!("Convert {number:?} from base {from_base} to base {to_base}")
 }
