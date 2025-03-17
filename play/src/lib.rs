d235fb7285980669f3287cc414155825d98277d4
Raphael Uziel <raphaeluziel@hotmail.com>
2025-03-17 17:34:42 -0400

    getting nowhere fast

--- play/src/lib.rs
+++ play/src/lib.rs
@@ -1,50 +1,21 @@
-use std::{collections::{HashMap, HashSet}, iter::Inspect};
+use std::collections::{HashMap, HashSet};
 
 pub fn solve(input: &str) -> Option<HashMap<char, u8>> {
-    let xx = input.replace(" ", "").replace("==", "=");
-    let mut hm: HashMap<char, u8> = HashMap::new();
-    let mut vs: Vec<u8> = Vec::new();
-    let mut decoded: Vec<u32> = Vec::new();
 
-    for x in xx.chars().filter(|&x| x != '+' && x != '=') {
+    let mut hm: HashMap<char, u8> = HashMap::new();
+    for x in input.chars().filter(|&x| x != '+' && x != '=' && x != ' ') {
         hm.insert(x, 1);
     }
+    println!("hash map = {:#?}", hm);
 
-    let mut iterations = 0;
-    loop {
-        decoded.clear();
-         
-        vs = HashSet::from([0, 1, 9])
-            .into_iter()
-            .collect();
-    
-        for (_, val) in hm.iter_mut() {
-            *val = vs.pop().unwrap_or_default();
-        }
-    
-        let coded_nums: Vec<&str> = xx.split(['+', '=']).collect();
-    
-        for alph in coded_nums {
-            let rrr: u32 = alph
-                .chars().rev()
-                .enumerate()
-                .map(|x| (*hm.get(&x.1).unwrap() as u32) * 10u32.pow(x.0 as u32))
-                .sum();
-            decoded.push(rrr);
-        }
+    let input = input.replace(" ", "").replace("==", "=");
+    let input: Vec<_> = input
+        .split(['+', '='])
+        .map(|x| x.chars().rev().collect::<String>())
+        .collect();
+    println!("Modified input as vector: {:?}", input);
 
-        let dede = decoded.pop().unwrap();
-        if dede == decoded.iter().sum() { println!("FOUND!"); break; }
-        
-        iterations += 1;
-        if iterations > 1000000 { return None; }
-    }
-
-    println!("\n-------------------------\nDecoded = {:?}", decoded);
-    println!("Iterations = {iterations}");
-    println!("HashMao = {:?}", hm);
+    //println!("first letters = {:?}", first_letters);
 
     Some(hm)
-
-    //todo!("Solve the alphametic {input:?}")
 }
