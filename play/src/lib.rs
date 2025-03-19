4b6f44a20cba148a2549808789e508a525fa8deb
Raphael Uziel <raphaeluziel@hotmail.com>
2025-03-19 17:18:21 -0400

    whatever

--- play/src/lib.rs
+++ play/src/lib.rs
@@ -7,7 +7,7 @@ pub fn solve(input: &str) -> Option<HashMap<char, u8>> {
     // for x in input.chars().filter(|&x| x != '+' && x != '=' && x != ' ') {
     //     hm.insert(x, 1);
     // }
-    println!("hash map = {:#?}", hm);
+    // println!("hash map = {:#?}", hm);
 
     let input = input.replace(" ", "").replace("==", "=");
 
@@ -15,7 +15,7 @@ pub fn solve(input: &str) -> Option<HashMap<char, u8>> {
         .split(['+', '='])
         .map(|x| x.chars().next().unwrap_or_default())
         .collect();
-    println!("First letters = {:?}", first_letters);
+    //println!("First letters = {:?}", first_letters);
 
     let mut input: Vec<Vec<char>> = input
         .split(['+', '='])
@@ -23,18 +23,45 @@ pub fn solve(input: &str) -> Option<HashMap<char, u8>> {
         .collect();
 
     let longest = input.iter().map(|x| x.len()).max().unwrap_or_default();
-    println!("longest = {longest}");
+    //println!("longest = {longest}");
 
     for v in &mut input {
         v.resize(longest, '.');
     }
 
-    println!("Modified input as vector: {:?}", input);
+    let summ = input.pop().unwrap_or_default();
+    //println!("summ = {:?}", summ);
 
-    let digits: Vec<u8> = (0..10).collect();
+    //println!("Modified input as vector: {:?}", input);
+
+    let mut digits: Vec<u8> = (0..10).collect();
     println!("Digits = {:?}", digits);
 
     let mut carry = 0;
+    let mut col = 0;
+
+    //println!("Input length = {:?}", input.len());
+
+    for col in 0..longest {
+        for row in 0..input.len() {
+            println!("({row}, {col}) ==>{:?}", input[row][col]);
+            if !hm.contains_key(&input[row][col]) && input[row][col] != '.' {
+                if first_letters.contains(&input[row][col]) && digits[0] == 0 {
+                    hm.insert(input[row][col], digits.remove(1));
+                    println!("A {:?}", hm);
+                    println!("digits = {:?}", digits);
+                } else {
+                    hm.insert(input[row][col], digits.remove(0));
+                    println!("A {:?}", hm);
+                    println!("digits = {:?}", digits);
+                }
+            }
+        }
+        println!("summ = {:?}\n", summ[col]);
+    }
+
+    println!("hash map = {:#?}", hm);
+    println!("digits = {:?}", digits);
 
     Some(hm)
 }
--- rust/alphametics/src/lib.rs
+++ rust/alphametics/src/lib.rs
@@ -1,5 +1,62 @@
-use std::collections::HashMap;
+// This probably won't pass.  
+// It uses brute force for one.  
+// I'm also not checking for repeating trials
+// I'm limiting it to 10_000_000 iterations, which may not cover
+// all possibilities due to repetitions.
+// I plan to try other solutions!!!!!!
+
+use std::collections::{HashMap, HashSet};
 
 pub fn solve(input: &str) -> Option<HashMap<char, u8>> {
-    todo!("Solve the alphametic {input:?}")
+    let input = input.replace(" ", "").replace("==", "=");
+    let mut hm: HashMap<char, u8> = HashMap::new();
+    let mut decoded: Vec<u64> = Vec::new();
+
+    for x in input.chars().filter(|&x| x != '+' && x != '=') {
+        hm.insert(x, 1);
+    }
+
+    let mut iterations = 0;
+    let coded_nums: Vec<&str> = input.split(['+', '=']).collect();
+    let first_letters: HashSet<char> = coded_nums
+        .iter()
+        .map(|x| x.chars().next().unwrap())
+        .collect();
+
+    'outer: loop {
+        decoded.clear();
+
+        let mut vs: Vec<u8> = HashSet::from([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+            .into_iter()
+            .collect();
+
+        for (key, val) in hm.iter_mut() {
+            let n = vs.pop().unwrap_or_default();
+            if n == 0 && first_letters.contains(key) { continue 'outer; }
+            *val = n;
+        }
+
+        for letter in &coded_nums {
+            decoded.push(
+                letter
+                    .chars()
+                    .rev()
+                    .enumerate()
+                    .map(|x| (*hm.get(&x.1).unwrap() as u64) * 10u64.pow(x.0 as u32))
+                    .sum(),
+            );
+        }
+
+        let dede = decoded.pop().unwrap();
+        if dede == decoded.iter().sum() {
+            break;
+        }
+
+        iterations += 1;
+        if iterations > 10_000_000 {
+            return None;
+        }
+    }
+
+    Some(hm)
 }
