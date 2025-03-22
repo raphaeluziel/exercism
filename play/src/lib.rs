3bdfcbb43c5f6efb1cb394d0c3981396e8ff8faf
raphaeluziel <raphaeluziel@gmail.com>
2025-03-22 18:19:21 -0400

    back to nowhere

--- play/src/lib.rs
+++ play/src/lib.rs
@@ -1,52 +1,49 @@
 use std::collections::{HashMap, HashSet};
 
-pub fn permutations(n: usize, v: &mut Vec<u8>, hm: &mut HashMap<char, u8>) {
+pub fn permutations(n: usize, permutation: &mut [u8; 10], hm: &mut HashMap<char, u8>) {
     if n == 1 {
-        //println!("Permute = {:?}", v);
+        //     for (key, val) in hm.iter_mut() {
+        //         let n = vs.pop().unwrap_or_default();
+        //         if n == 0 && first_letters.contains(key) {
+        //             continue 'outer;
+        //         }
+        //         *val = n;
+        //     }
+        for x in hm.iter_mut().enumerate() {
+            //println!("JKJKJKJK = {:?}", x);
+            *x.1.1 = permutation[x.0];
+        }
         return;
     } else {
-        permutations(n - 1, v, hm);
+        permutations(n - 1, permutation, hm);
 
         for i in 0..(n - 1) {
             if i % 2 == 0 {
-                let temp = v[i];
-                v[i] = v[n - 1];
-                v[n - 1] = temp;
+                let temp = permutation[i];
+                permutation[i] = permutation[n - 1];
+                permutation[n - 1] = temp;
             } else {
-                let temp = v[0];
-                v[0] = v[n - 1];
-                v[n - 1] = temp;
+                let temp = permutation[0];
+                permutation[0] = permutation[n - 1];
+                permutation[n - 1] = temp;
             }
-            permutations(n - 1, v, hm);
+            permutations(n - 1, permutation, hm);
         }
     }
 }
 
 pub fn solve(input: &str) -> Option<HashMap<char, u8>> {
-
     let mut permutation = [0u8, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-    // let len = permutation.len();
-    // for i in 0..len {
-    //     if permutation[len - i - 2] < permutation[len - i - 1] {
-    //         let g = &permutation[(len - i - 1)..(len - 1)].iter().position(|x| x );
-    //     }
-    // }
-
-    let mut v: Vec<u8> = (0..4).collect();
-    permutations(v.len(), &mut v);
+    println!("PP = {:?}", permutation);
 
     let input = input.replace(" ", "").replace("==", "=");
     let mut hm: HashMap<char, u8> = HashMap::new();
     let mut decoded: Vec<u64> = Vec::new();
-    let mut vs: Vec<u8> = (0..10).collect();
+    //let mut vs: Vec<u8> = (0..10).collect();
 
     for x in input.chars().filter(|&x| x != '+' && x != '=') {
         hm.insert(x, 1);
     }
-
-    // for p in 0..10 {
-    //     println!("HM = {:?}", hm);
-    // }
 
     let mut iterations = 0;
     let coded_nums: Vec<&str> = input.split(['+', '=']).collect();
@@ -55,15 +52,37 @@ pub fn solve(input: &str) -> Option<HashMap<char, u8>> {
         .map(|x| x.chars().next().unwrap())
         .collect();
 
+    // println!("original hashmap = {:?}", hm);
+    // permutations(permutation.len(), &mut permutation, &mut hm);
+    // println!("Now the  hashmap = {:?}", hm);
+    // loop {
+    //     let gh = hm.iter().find(|x| first_letters.contains(x.0) && *x.1 == 0);
+    //     match gh {
+    //         None => break,
+    //         _ => permutations(permutation.len(), &mut permutation, &mut hm)
+    //     }
+    // }
+    println!("Finall   hashmap = {:?}", hm);
+
     'outer: loop {
         decoded.clear();
 
-        for (key, val) in hm.iter_mut() {
-            let n = vs.pop().unwrap_or_default();
-            if n == 0 && first_letters.contains(key) {
-                continue 'outer;
+        // for (key, val) in hm.iter_mut() {
+        //     let n = vs.pop().unwrap_or_default();
+        //     if n == 0 && first_letters.contains(key) {
+        //         continue 'outer;
+        //     }
+        //     *val = n;
+        // }
+
+        permutations(permutation.len(), &mut permutation, &mut hm);
+        loop {
+            let gh = hm.iter().find(|x| first_letters.contains(x.0) && *x.1 == 0);
+            println!("gh = {:?}", gh);
+            match gh {
+                None => break,
+                _ => continue 'outer,
             }
-            *val = n;
         }
 
         for letter in &coded_nums {
@@ -86,6 +105,6 @@ pub fn solve(input: &str) -> Option<HashMap<char, u8>> {
     }
     println!("iterations = {iterations}");
 
-    Some(hm)
-    //todo!("hey")
+    //Some(hm)
+    todo!("hey")
 }
