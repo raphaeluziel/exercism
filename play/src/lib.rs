a00b0f38e3f89b89db24951affa3a39c03e2f895
raphaeluziel <raphaeluziel@gmail.com>
2025-03-29 11:25:26 -0400

    getting nowhere

--- play/src/lib.rs
+++ play/src/lib.rs
@@ -1,36 +1,39 @@
-use std::{borrow::Borrow, cmp::Ordering, ops::Index, process::Output, slice::SliceIndex, usize};
+use std::{borrow::Borrow, cmp::Ordering};
 
-pub fn find<U, T>(array: U, key: T) -> Option<usize> 
-    where T: std::cmp::PartialEq + Ord, 
-          U: Borrow<[T]> + ExactSizeIterator + Index<usize>
-
+pub fn find<U, T>(array: U, key: T) -> Option<usize>
+    where U: Borrow<[T]> + ExactSizeIterator + std::fmt::Debug,
+          T: PartialEq + Ord
 {
-    if array.len() == 0 { return None; }
-    if array.len() == 1 {
-        if key == array[0] { return Some(0); }
-        else { return None; }
+    let x = array.into_iter();
+    for c in &x {
+        println!("{:?}", x);
     }
+    // if array.len() == 0 { return None; }
+    // if array.len() == 1 {
+    //     if key == array[0] { return Some(0); }
+    //     else { return None; }
+    // }
 
-    let mut start:usize = 0;
-    let mut end:usize = array.len() - 1;
-    let mut mid:usize = (end - start) / 2;
+    // let mut start:usize = 0;
+    // let mut end:usize = array.len() - 1;
+    // let mut mid:usize = (end - start) / 2;
 
-    if key == array[start] { return Some(start); }
-    if key == array[end] { return  Some(end); }
+    // if key == array[start] { return Some(start); }
+    // if key == array[end] { return  Some(end); }
 
-    while mid != start && mid != end {
-        match key.cmp(&array[mid]) {
-            Ordering::Equal => return Some(mid),
-            Ordering::Less => { 
-                end = mid;
-                mid -= (end - start) / 2;
-            },
-            Ordering::Greater => {
-                start = mid;
-                mid += (end - start) / 2;
-            }
-        };
-    }
+    // while mid != start && mid != end {
+    //     match key.cmp(&array[mid]) {
+    //         Ordering::Equal => return Some(mid),
+    //         Ordering::Less => { 
+    //             end = mid;
+    //             mid -= (end - start) / 2;
+    //         },
+    //         Ordering::Greater => {
+    //             start = mid;
+    //             mid += (end - start) / 2;
+    //         }
+    //     };
+    // }
     
     None
 }
