c87224306adead6d414fa5fefbefeaa9651f0636
raphaeluziel <raphaeluziel@gmail.com>
2025-04-10 11:28:42 -0400

    need newer approach

--- play/src/lib.rs
+++ play/src/lib.rs
@@ -8,17 +8,17 @@ pub struct BowlingGame {
     score: u16,
     bonus: u16,
     last_roll: u16,
-    rolls: u16,
+    rolled: u16,
     frame: u16,
 }
 
 impl BowlingGame {
     pub fn new() -> Self {
         BowlingGame { 
-            score: 0, 
+            score: 0,
             bonus: 0,
             last_roll: 0,
-            rolls: 0,
+            rolled: 0,
             frame: 0,
         }
     }
@@ -39,23 +39,25 @@ impl BowlingGame {
             self.last_roll = 10;
             self.bonus += 2;
             self.frame += 1;
-            self.frame_start = true;
+            self.rolled = if self.frame != 10 { 0 } else { self.rolled + 1 }
         }
         else {
-            if self.frame_start {
+            if self.rolled == 0 {
                 self.score += pins;
                 self.last_roll = pins;
-                self.frame_start = false;
-                self.fill_balls = 0;
+                self.rolled = 1;
             }
-            else {
+            else if self.rolled == 1 {
                 let frame_total = pins + self.last_roll;
                 if frame_total > 10 { return Err(Error::NotEnoughPinsLeft); }
                 if frame_total == 10 { self.bonus += 1; }
                 self.score += pins;
                 self.last_roll = 0;
                 self.frame += 1;
-                self.frame_start = true;
+                self.rolled = if self.frame != 10 { 0 } else { 2 }
+            }
+            else {
+
             }
         }
         
