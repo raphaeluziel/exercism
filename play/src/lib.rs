8930b079d96f594916a0bea91932ab13ca0a8d16
Raphael Uziel <raphaeluziel@hotmail.com>
2025-04-16 17:14:15 -0400

    nothing really

--- play/src/lib.rs
+++ play/src/lib.rs
@@ -3,13 +3,42 @@ use std::collections::BTreeMap;
 pub fn transform(h: &BTreeMap<i32, Vec<char>>) -> BTreeMap<char, i32> {
     let mut b:BTreeMap<char, i32> = BTreeMap::new();
 
-    let g:BTreeMap<char, i32> = h.iter().map(|(k, v)| )
+    // println!("{:?}", h[&4]);
+
+    let jj = h.iter()
+                     .map(|x| x.1.iter().map(move |&y| (y, x.0)).collect::<Vec<_>>());
+                     //.inspect(|x| println!("outer = {:?}", x))
+                     //.collect();
+
+    println!();
+
+    let gg:Vec<_> = h[&4].iter()
+                         .map(|&x| (x, 4))
+                         .inspect(|x| println!("inner = {:?}", x))
+                         .collect();
+
+    println!("\nSO FAR\n{:?}", jj);
+
+    //let rr = BTreeMap::from(jj);
+    //println!("FINAL? {:?}", rr);
 
     // for k in h {
     //     for x in k.1 {
     //         b.insert((*x).to_ascii_lowercase(), *k.0);
     //     }
     // }
+
+
+
+    // let input = BTreeMap::from([
+    //     (1, vec!['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T']),
+    //     (2, vec!['D', 'G']),
+    //     (3, vec!['B', 'C', 'M', 'P']),
+    //     (4, vec!['F', 'H', 'V', 'W', 'Y']),
+    //     (5, vec!['K']),
+    //     (8, vec!['J', 'X']),
+    //     (10, vec!['Q', 'Z']),
+    // ]);
 
     b
 }
