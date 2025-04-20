d5900394a9aed29cc64736f2a3a38e77ad248afe
Raphael Uziel <raphaeluziel@hotmail.com>
2025-04-20 17:15:55 -0400

    working on isbn

--- play/src/lib.rs
+++ play/src/lib.rs
@@ -1,4 +1,14 @@
 /// Determines whether the supplied string is a valid ISBN number
 pub fn is_valid_isbn(isbn: &str) -> bool {
-    todo!("Is {isbn:?} a valid ISBN number?");
+
+    
+    
+    isbn.chars().rev()
+        .filter(|&ch| ch.is_ascii_digit() || ch == 'X')
+        .enumerate()
+        .inspect(|x| println!("X = {:?}", x))
+        .fold(0, |acc, (i, ch)| acc + 
+                    (i as u32 + 1) * ch.to_digit(10)
+                .unwrap_or(10)) 
+        % 11 == 0  
 }
