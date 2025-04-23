42c3e794aeab1d69386d5e145b531368aebcb6d7
Raphael Uziel <raphaeluziel@hotmail.com>
2025-04-23 17:34:48 -0400

    nocapture

--- play/src/lib_original.rs
+++ play/src/lib_original.rs
@@ -3,52 +3,43 @@ use std::io::{Read, Result, Write};
 // the PhantomData instances in this file are just to stop compiler complaints
 // about missing generics; feel free to remove them
 
-pub struct ReadStats<R> {
-    read_data: R,
-    buffer: Vec<u8>
-}
+pub struct ReadStats<R>(::std::marker::PhantomData<R>);
 
 impl<R: Read> ReadStats<R> {
     // _wrapped is ignored because R is not bounded on Debug or Display and therefore
     // can't be passed through format!(). For actual implementation you will likely
     // wish to remove the leading underscore so the variable is not ignored.
-    pub fn new(wrapped: R) -> ReadStats<R> {
-        ReadStats { read_data: wrapped, buffer: Vec::new() }
+    pub fn new(_wrapped: R) -> ReadStats<R> {
+        todo!()
     }
 
     pub fn get_ref(&self) -> &R {
-        &self.read_data
+        todo!()
     }
 
     pub fn bytes_through(&self) -> usize {
-        42
+        todo!()
     }
 
     pub fn reads(&self) -> usize {
-        42
+        todo!()
     }
 }
 
 impl<R: Read> Read for ReadStats<R> {
     fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
-        println!("HEY");
-        self.buffer = buf.to_vec();
-        println!("SELFIE {:?}", self.buffer);
-        Ok(self.buffer.len())
-        //todo!("Collect statistics about this call reading {buf:?}")
+        todo!("Collect statistics about this call reading {buf:?}")
     }
 }
 
-pub struct WriteStats<W> {
-    write_data: W,
-}
+pub struct WriteStats<W>(::std::marker::PhantomData<W>);
 
 impl<W: Write> WriteStats<W> {
     // _wrapped is ignored because W is not bounded on Debug or Display and therefore
     // can't be passed through format!(). For actual implementation you will likely
     // wish to remove the leading underscore so the variable is not ignored.
-    pub fn new(wrapped: W) -> WriteStats<W> {
-        WriteStats { write_data: wrapped, }
+    pub fn new(_wrapped: W) -> WriteStats<W> {
+        todo!()
     }
 
     pub fn get_ref(&self) -> &W {
--- play/tests/play.rs
+++ play/tests/play.rs
@@ -1,6 +1,7 @@
 use std::io::{Error, ErrorKind, Read, Result, Write};
 
 #[test]
+#[ignore]
 fn create_stats() {
     let mut data: Vec<u8> = Vec::new();
     let _ = play::ReadStats::new(data.as_slice());
@@ -16,7 +17,7 @@ mod read_string {
     static INPUT: &[u8] = b"Twas brillig, and the slithy toves/Did gyre and gimble in the wabe:/All mimsy were the borogoves,/And the mome raths outgrabe.";
 
     #[test]
-    #[ignore]
+
     fn read_passthrough() {
         let data = INPUT;
         let size = data.len();
