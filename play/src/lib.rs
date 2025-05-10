d75c47a566defd135343590713ecf32fc7b47dcf
raphaeluziel <raphaeluziel@gmail.com>
2025-05-10 18:45:28 -0400

    looking good

--- play/src/lib.rs
+++ play/src/lib.rs
@@ -13,28 +13,42 @@ impl PascalsTriangle {
         triangle.push(vec![1, 3, 3, 1]);
         triangle.push(vec![1, 4, 6, 4, 1]);
 
-        let mut v = vec![1; 4];
-        v.insert(1, triangle[4][0] + triangle[4][1]);
-        v.insert(2, triangle[4][1] + triangle[4][2]);
-        
+        // let mut v = vec![1; 4];
+        // v.insert(1, triangle[4][0] + triangle[4][1]);
+        // v.insert(2, triangle[4][1] + triangle[4][2]);
 
-        // let mut v:Vec<u32> = Vec::new();
-        // v.push(1);
-        // v.push(triangle[4][0] + triangle[4][1]);
-        // v.push(triangle[4][1] + triangle[4][2]);
-        
-        //v.rotate_left(2);
-        // v.push(triangle[4][2] + triangle[4][3]);
-        // v.push(triangle[4][3] + triangle[4][4]);
-        // v.push(1);
-        
+        let mut v:Vec<u32> = Vec::new();
+        v.push(1);
+        v.push(triangle[4][0] + triangle[4][1]);
+        v.push(triangle[4][1] + triangle[4][2]);
+        v.push(triangle[4][1] + triangle[4][2]);
+        v.push(triangle[4][0] + triangle[4][1]);
+        v.push(1);
+
+        let mut ggg = vec![1u32];
+        ggg.extend(triangle[4].windows(2).map(|x| x[0] + x[1]));
+        ggg.push(1);
+        println!("GGG = {:?}", ggg);
+
+        triangle.push(v.clone());
+
+        let mut w:Vec<u32> = Vec::new();
+        w.push(1);
+        w.push(triangle[5][0] + triangle[5][1]);
+        w.push(triangle[5][1] + triangle[5][2]);
+        w.push(triangle[5][2] + triangle[5][3]);
+        w.push(triangle[5][1] + triangle[5][2]);
+        w.push(triangle[5][0] + triangle[5][1]);
+        w.push(1);
+
         println!("Sizes = {} and {}", triangle[4].len()/2, v.len()/2);
         println!("Row 4 = {:?}", triangle[4]);
         println!("Row 5 = [1, 5, 10, 10, 5, 1]");
         println!("Row 5 = {:?}", v);
 
-
-        PascalsTriangle { triangle }
+        println!("W = {:?}", w);
+        //PascalsTriangle { triangle }
+        todo!()
     }
 
     pub fn rows(&self) -> Vec<Vec<u32>> {
