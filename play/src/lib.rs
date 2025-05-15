f13c11e9a5659120399622d5b6373316b3658f28
Raphael Uziel <raphaeluziel@hotmail.com>
2025-05-15 17:09:50 -0400

    progress

--- play/src/lib.rs
+++ play/src/lib.rs
@@ -2,6 +2,8 @@ pub fn translate(input: &str) -> String {
 
     let mut input = input.to_ascii_lowercase();
 
+    let mut input = "square".to_string();   // aresquay
+
     let vowel = ['a', 'e', 'i', 'o', 'u'];
     let rule_1 = ["a", "e", "i", "o", "u", "xr", "yt"];
     let rule_2 = ["a", "e", "i", "o", "u"];
@@ -9,20 +11,16 @@ pub fn translate(input: &str) -> String {
     if rule_1.iter().any(|&x| input.starts_with(x)) { 
         input += "ay";
     }
-    else if !rule_2.iter().any(|&x| input.starts_with(x)) {
-        let vvv:Vec<_> = input.split
+    else if !rule_2.iter().any(|&x| input.starts_with(x))  && !input.contains("qu") {
+        let i = input.find(vowel).unwrap();
+        input = input.get(i..).unwrap().to_owned() + input.get(0..i).unwrap() + "ay";
     }
-    // else if input.find("qu") != None {
-    //     let vvv:Vec<_> = input.split("qu").collect();
-    //     let ggg = vvv[0].find(vowel);
-    //     let mut fff = "";
-    //     if ggg != None {
-    //         fff = vvv[0].get(0..ggg.unwrap()).unwrap();
-    //         println!("FFF = {:?}", fff);
-    //     }
-    //     println!("vvv = {:?}", vvv);
-    //     input = vvv[1].to_owned() + fff + "qu" + "ay";
-    // }
+    else if !rule_2.iter().any(|&x| input.starts_with(x)) && input.contains("qu") {
+        println!("HERE");
+        let i = input.find(vowel).unwrap();
+        let j = input.find("qu").unwrap();
+        input = input.get(i..j).unwrap().to_owned() + input.get(0..i).unwrap() + "quay";
+    }
 
     println!("CCC = {:?}", input);
 
