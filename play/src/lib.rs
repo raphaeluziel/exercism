53c9006c11c7597e47e235f0ae111c1a1019a833
raphaeluziel <raphaeluziel@gmail.com>
2025-05-17 18:17:38 -0400

    nothing at all

--- play/src/lib.rs
+++ play/src/lib.rs
@@ -1,14 +1,30 @@
 pub fn translate(input: &str) -> String {
 
     let input = input.to_ascii_lowercase();
+    let input = "square";
 
-    let v = input.find(['a', 'e', 'i', 'o', 'u', 'x', 'y', 'q']);
-    println!("{input}: {:?}", v);
+    let vowels = ['a', 'e', 'i', 'o', 'u'];
 
-    match v {
-        Some(0) => println!("Rule 1"),
-        
+    let mut index: Option<usize> = None;
+
+    if input.starts_with(vowels) 
+        || input.starts_with("xr") 
+        || input.starts_with("yt") { 
+            index = Some(0); 
     }
+    else { 
+        index = input.find(vowels);
+        if index != None {
+
+        }
+    }
+
+    // let i = input.as_bytes()
+    //              .windows(2)
+    //              .enumerate()
+    //              .find(|&(i, x)| (vowels.contains(&x[0]) || (&x[0] == b'x' ) ));
+
+    println!("i = {:?}", index);
 
     todo!()
 }
