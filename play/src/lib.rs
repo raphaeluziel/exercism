ef02ce4fc57be4f5ba83c093cbf8e1527181d375
raphaeluziel <raphaeluziel@gmail.com>
2025-05-26 11:10:38 -0400

    dna done

--- play/src/lib.rs
+++ play/src/lib.rs
@@ -1,25 +1,39 @@
 #[derive(Debug, PartialEq, Eq)]
-pub struct Dna;
+pub struct Dna {
+    dna: String
+}
 
 #[derive(Debug, PartialEq, Eq)]
-pub struct Rna;
+pub struct Rna {
+    rna: String
+}
 
 impl Dna {
     pub fn new(dna: &str) -> Result<Dna, usize> {
-        todo!(
-            "Construct new Dna from '{dna}' string. If string contains invalid nucleotides return index of first invalid nucleotide"
-        );
+        match dna.find(|ch| !['A', 'C', 'G', 'T'].contains(&ch)) {
+            Some(i) => Err(i),
+            None => Ok(Dna { dna: dna.to_string() })
+        }
     }
 
     pub fn into_rna(self) -> Rna {
-        todo!("Transform Dna {self:?} into corresponding Rna");
+        Rna { rna: self.dna.chars().map(|d| 
+            match d {
+                'G' => 'C',
+                'C' => 'G',
+                'T' => 'A',
+                'A' => 'U',
+                _ => '\0'
+            }
+        ).collect() }
     }
 }
 
 impl Rna {
     pub fn new(rna: &str) -> Result<Rna, usize> {
-        todo!(
-            "Construct new Rna from '{rna}' string. If string contains invalid nucleotides return index of first invalid nucleotide"
-        );
+        match rna.find(|ch| !['A', 'C', 'G', 'U'].contains(&ch)) {
+            Some(i) => Err(i),
+            None => Ok(Rna { rna: rna.to_string() })
+        }
     }
 }
