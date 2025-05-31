72bc4102ad05663db1c59d2259a0208594cda13b
raphaeluziel <raphaeluziel@gmail.com>
2025-05-31 10:51:18 -0400

    run-length-encoder solved

--- play/src/lib.rs
+++ play/src/lib.rs
@@ -18,31 +18,33 @@ pub fn encode(source: &str) -> String {
     n += 1;
     if n > 1 { s.push_str(&n.to_string()) }
     s.push(curr);
-    println!("input  = {source}");
-    println!("output = {:?}", s);
     s
 }
 
 pub fn decode(source: &str) -> String {
     let mut s: String = String::new();
     if source.is_empty() { return s; }
-
-    let mut letters = source.chars();
-    let mut n: u64 = 0;
-    let mut curr = letters.next().unwrap_or_default();
-    let mut prev = false;
+    
+    let letters = source.chars();
+    let mut n = 0;
     let mut power = 1;
 
     for letter in letters {
         if letter.is_ascii_digit() {
-            if prev {
-                power *= 10;
-                n += power * letter.to_digit(10).unwrap_or_default();
-            }
-            
-
+            n = power * n + letter.to_digit(10).unwrap_or_default();
+            power *= 10;   
         }
+        else {
+            if n == 0 {
+                n = 1;
+            }
+            for _ in 0..n {
+                s.push(letter);
+            }
+            power = 1;
+            n = 0;
+        } 
     }
 
-    todo!("Return the run-length decoding of {source}.");
+    s
 }
