7e69a1362dd226ada8565ff17af610e67eb7b772
raphaeluziel <raphaeluziel@gmail.com>
2025-06-03 11:28:25 -0400

    nothing

--- play/src/lib.rs
+++ play/src/lib.rs
@@ -2,22 +2,51 @@ pub fn encode(source: &str) -> String {
     let mut s: String = String::new();
     if source.is_empty() { return s; }
 
-    let mut letters = source.chars();
-    let mut n: u64 = 0;
-    let mut curr = letters.next().unwrap_or_default();
+    let source = "WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWBBBBBBBBBB";
 
-    for letter in letters {
+    let mut n = 0;
+
+    for x in source.as_bytes().windows(2) {
+        println!("{:?}", x);
         n += 1;
-        if letter != curr {
-            if n > 1 { s.push_str(&n.to_string()) }
-            s.push(curr);
-            curr = letter;
+        if x[0] != x[1] {
+            if n > 1 { s.push_str(&n.to_string()); }
+            s.push(x[0] as char);
             n = 0;
         }
     }
-    n += 1;
-    if n > 1 { s.push_str(&n.to_string()) }
-    s.push(curr);
+
+    // for x in source.as_bytes().windows(2).peekable() {
+    //     println!("{:?}", x);
+    //     if x[0] == x[1] {
+    //         n += 1;
+    //     }
+    //     else {
+    //         if n > 1 { s.push_str(&(n + 1).to_string()); }
+    //         s.push(x[0] as char);
+    //         n = 0;
+    //     }
+    // }
+
+
+    println!("s = {:?}", s);
+
+    // let mut letters = source.chars();
+    // let mut n: u64 = 0;
+    // let mut curr = letters.next().unwrap_or_default();
+
+    // for letter in letters {
+    //     n += 1;
+    //     if letter != curr {
+    //         if n > 1 { s.push_str(&n.to_string()) }
+    //         s.push(curr);
+    //         curr = letter;
+    //         n = 0;
+    //     }
+    // }
+    // n += 1;
+    // if n > 1 { s.push_str(&n.to_string()) }
+    // s.push(curr);
     s
 }
 
