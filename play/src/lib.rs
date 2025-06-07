389990845c7c069621f266410d1de69037065ed9
Raphael Uziel <raphaeluziel@hotmail.com>
2025-06-07 17:27:43 -0400

    nothing

--- play/src/lib.rs
+++ play/src/lib.rs
@@ -2,7 +2,19 @@ pub fn find_saddle_points(input: &[Vec<u64>]) -> Vec<(usize, usize)> {
 
     let input = &[vec![9, 8, 7, 8, 9, 8], vec![5, 3, 5, 1, 5, 2], vec![6, 6, 7, 1, 1, 1]];
 
-    let columns: Vec<Vec<u64>> = Vec::with_capacity(input.len());
+    let num_rows = input.len();
+    let num_cols = input[0].len();
+
+    println!("{num_rows} rows, {num_cols} columns");
+
+    for j in 0..num_cols {
+        for i in 0..num_rows {
+            println!("({i}, {j}) -> {:?}", input[i][j]);
+        }
+        println!();
+    }
+
+    //let columns: Vec<Vec<u64>> = Vec::with_capacity(input.len());
 
     // for row in input {
     //     columns.push();
