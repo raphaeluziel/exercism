3fbb102a6d8ab48754c0f8917c0d93c310344e72
raphaeluziel <raphaeluziel@gmail.com>
2025-06-14 11:44:47 -0400

    leaving

--- play/src/lib.rs
+++ play/src/lib.rs
@@ -1,71 +1,108 @@
-use std::string;
+// const ZEROS: [char; 10] = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '];
+// const ONES: [char; 10] = ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'];
+// const TWOS: [char; 10] = ['D', 'G', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '];
+// const THREES: [char; 10] = ['B', 'C', 'M', 'P', ' ', ' ', ' ', ' ', ' ', ' '];
+// const FOURS: [char; 10] = ['F', 'H', 'V', 'W', 'Y', ' ', ' ', ' ', ' ', ' '];
+// const FIVES: [char; 10] = ['K', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '];
+// const EIGHTS: [char; 10] = ['J', 'K', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '];
+// const TENS: [char; 10] = ['Q', 'Z', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '];
 
-const DIGITS: [&str; 10] = [
-    "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
-];
+// const LETTER_VALUES: [[char; 10]; 11] = [
+//     ZEROS, ONES, TWOS, THREES, FOURS, FIVES, ZEROS, ZEROS, EIGHTS, ZEROS, TENS,
+// ];
 
-const TENS: [&str; 10] = [
-    "", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety",
-];
+use std::collections::HashMap;
 
-const TEENS: [&str; 10] = [
-    "ten",
-    "eleven",
-    "twelve",
-    "thirteen",
-    "fourteen",
-    "fifteen",
-    "sixteen",
-    "seventeen",
-    "eighteen",
-    "nineteen",
-];
+pub fn score(word: &str) -> u64 {
+    let ones = "AEIOULNRST";
+    let twos = "DG";
+    let threes = "BCMP";
+    let fiours = "FHVWY";
+    let fives = "K";
+    let eights = "JX";
+    let tens = "QZ";
+    
+    let ccc: HashMap<char, u64> = ones.chars().map(|ch| 
+        match  {
+            'A'| 'E'| 'I'| 'O'| 'U'| 'L'| 'N'| 'R'| 'S'| 'T' => 1,
 
-const BREAKERS: [&str; 7] = [
-    "",
-    "thousand",
-    "million",
-    "billion",
-    "trillion",
-    "quadrillion",
-    "quintillion",
-];
+        }
+        )
+        .collect();
+    println!("ccc = {:?}", ccc);
 
-fn say_triple(n: usize) -> String {
-    if n > 99 { DIGITS[n / 100].to_string() + " hundred " + &say_double(n % 100) }
-    else { say_double(n) }
+    // word.chars().fold(0, |acc, x| )
+    todo!("Score {word} in Scrabble.");
 }
 
-fn say_double(n: usize) -> String {
-    match n {
-        0..10 => DIGITS[n].to_string(),
-        10..20 => TEENS[n - 10].to_string(),
-        x if x % 10 == 0 => TENS[n / 10].to_string(),
-        _ => TENS[n / 10].to_string() + "-" + DIGITS[n % 10]
-    }
-}
+// const DIGITS: [&str; 10] = [
+//     "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+// ];
 
-pub fn encode(n: u64) -> String {
-    println!("YOYO = {}", say_double(0));
-    let mut s = String::new();
-    let mut num = n;
+// const TENS: [&str; 10] = [
+//     "", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety",
+// ];
 
-    let mut threes: Vec<u64> = Vec::new();
+// const TEENS: [&str; 10] = [
+//     "ten",
+//     "eleven",
+//     "twelve",
+//     "thirteen",
+//     "fourteen",
+//     "fifteen",
+//     "sixteen",
+//     "seventeen",
+//     "eighteen",
+//     "nineteen",
+// ];
 
-    while num > 0 {
-        threes.push(num % 1000);
-        num /= 1000;
-    }
+// const BREAKERS: [&str; 7] = [
+//     "",
+//     "thousand",
+//     "million",
+//     "billion",
+//     "trillion",
+//     "quadrillion",
+//     "quintillion",
+// ];
 
-    threes.reverse();
+// fn say_triple(n: usize) -> String {
+//     if n == 0 { "".to_string() }
+//     else if n > 99 { DIGITS[n / 100].to_string() + " hundred " + &say_double(n % 100) }
+//     else { say_double(n) }
+// }
 
-    let mut breaker = threes.len();
+// fn say_double(n: usize) -> String {
+//     match n {
+//         0 => "".to_string(),
+//         1..10 => DIGITS[n].to_string(),
+//         10..20 => TEENS[n - 10].to_string(),
+//         x if x % 10 == 0 => TENS[n / 10].to_string(),
+//         _ => TENS[n / 10].to_string() + "-" + DIGITS[n % 10]
+//     }
+// }
 
-    for triple in &threes {
-        breaker -= 1;
-        s += &(say_triple(*triple as usize) + " " + BREAKERS[breaker] + "");
-    }
+// pub fn encode(n: u64) -> String {
+//     if n == 0 { return "zero".to_string(); }
 
-    //s.trim().to_string()
-    todo!()
-}
+//     let mut s = String::new();
+
+//     let mut num = n;
+//     let mut threes: Vec<u64> = Vec::new();
+
+//     while num > 0 {
+//         threes.push(num % 1000);
+//         num /= 1000;
+//     }
+
+//     threes.reverse();
+
+//     let mut breaker = threes.len();
+
+//     for triple in &threes {
+//         breaker -= 1;
+//         if *triple != 0 { s += &(say_triple(*triple as usize) + " " + BREAKERS[breaker] + " ") }
+//     }
+
+//     s.trim().to_string()
+// }
--- play/tests/play.rs
+++ play/tests/play.rs
@@ -1,172 +1,289 @@
 use play::*;
 
 #[test]
-#[ignore]
-fn zero() {
-    let input = 0;
-    let output = encode(input);
-    let expected = "zero";
+fn lowercase_letter() {
+    let input = "a";
+    let output = score(input);
+    let expected = 1;
     assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
-fn one() {
-    let input = 1;
-    let output = encode(input);
-    let expected = "one";
+fn uppercase_letter() {
+    let input = "A";
+    let output = score(input);
+    let expected = 1;
     assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
-fn fourteen() {
-    let input = 14;
-    let output = encode(input);
-    let expected = "fourteen";
+fn valuable_letter() {
+    let input = "f";
+    let output = score(input);
+    let expected = 4;
     assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
-fn twenty() {
-    let input = 20;
-    let output = encode(input);
-    let expected = "twenty";
-    assert_eq!(output, expected);
-}
-
-#[test]
-//#[ignore]
-fn twenty_two() {
-    let input = 22;
-    let output = encode(input);
-    let expected = "twenty-two";
+fn short_word() {
+    let input = "at";
+    let output = score(input);
+    let expected = 2;
     assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
-fn thirty() {
-    let input = 30;
-    let output = encode(input);
-    let expected = "thirty";
+fn short_valuable_word() {
+    let input = "zoo";
+    let output = score(input);
+    let expected = 12;
     assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
-fn ninety_nine() {
-    let input = 99;
-    let output = encode(input);
-    let expected = "ninety-nine";
+fn medium_word() {
+    let input = "street";
+    let output = score(input);
+    let expected = 6;
     assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
-fn one_hundred() {
-    let input = 100;
-    let output = encode(input);
-    let expected = "one hundred";
+fn medium_valuable_word() {
+    let input = "quirky";
+    let output = score(input);
+    let expected = 22;
     assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
-fn one_hundred_twenty_three() {
-    let input = 123;
-    let output = encode(input);
-    let expected = "one hundred twenty-three";
+fn long_mixed_case_word() {
+    let input = "OxyphenButazone";
+    let output = score(input);
+    let expected = 41;
     assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
-fn two_hundred() {
-    let input = 200;
-    let output = encode(input);
-    let expected = "two hundred";
+fn english_like_word() {
+    let input = "pinata";
+    let output = score(input);
+    let expected = 8;
     assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
-fn nine_hundred_ninety_nine() {
-    let input = 999;
-    let output = encode(input);
-    let expected = "nine hundred ninety-nine";
+fn empty_input() {
+    let input = "";
+    let output = score(input);
+    let expected = 0;
     assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
-fn one_thousand() {
-    let input = 1_000;
-    let output = encode(input);
-    let expected = "one thousand";
+fn entire_alphabet_available() {
+    let input = "abcdefghijklmnopqrstuvwxyz";
+    let output = score(input);
+    let expected = 87;
     assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
-fn one_thousand_two_hundred_thirty_four() {
-    let input = 1_234;
-    let output = encode(input);
-    let expected = "one thousand two hundred thirty-four";
+fn non_english_scrabble_letters_do_not_score() {
+    let input = "piñata";
+    let output = score(input);
+    let expected = 7;
     assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
-fn one_million() {
-    let input = 1_000_000;
-    let output = encode(input);
-    let expected = "one million";
+fn german_letters_do_not_score() {
+    let input = "STRAßE";
+    let output = score(input);
+    let expected = 5;
     assert_eq!(output, expected);
 }
 
-#[test]
-#[ignore]
-fn one_million_two_thousand_three_hundred_forty_five() {
-    let input = 1_002_345;
-    let output = encode(input);
-    let expected = "one million two thousand three hundred forty-five";
-    assert_eq!(output, expected);
-}
 
-#[test]
-#[ignore]
-fn one_billion() {
-    let input = 1_000_000_000;
-    let output = encode(input);
-    let expected = "one billion";
-    assert_eq!(output, expected);
-}
+// #[test]
+// #[ignore]
+// fn zero() {
+//     let input = 0;
+//     let output = encode(input);
+//     let expected = "zero";
+//     assert_eq!(output, expected);
+// }
 
-#[test]
-#[ignore]
-fn a_big_number() {
-    let input = 987_654_321_123;
-    let output = encode(input);
-    let expected = "nine hundred eighty-seven billion six hundred fifty-four million three hundred twenty-one thousand one hundred twenty-three";
-    assert_eq!(output, expected);
-}
+// #[test]
+// #[ignore]
+// fn one() {
+//     let input = 1;
+//     let output = encode(input);
+//     let expected = "one";
+//     assert_eq!(output, expected);
+// }
 
-#[test]
-#[ignore]
-fn max_i64() {
-    let input = 9_223_372_036_854_775_807;
-    let output = encode(input);
-    let expected = "nine quintillion two hundred twenty-three quadrillion three hundred seventy-two trillion thirty-six billion eight hundred fifty-four million seven hundred seventy-five thousand eight hundred seven";
-    assert_eq!(output, expected);
-}
+// #[test]
+// #[ignore]
+// fn fourteen() {
+//     let input = 14;
+//     let output = encode(input);
+//     let expected = "fourteen";
+//     assert_eq!(output, expected);
+// }
 
-#[test]
-#[ignore]
-fn max_u64() {
-    let input = 18_446_744_073_709_551_615;
-    let output = encode(input);
-    let expected = "eighteen quintillion four hundred forty-six quadrillion seven hundred forty-four trillion seventy-three billion seven hundred nine million five hundred fifty-one thousand six hundred fifteen";
-    assert_eq!(output, expected);
-}
+// #[test]
+// #[ignore]
+// fn twenty() {
+//     let input = 20;
+//     let output = encode(input);
+//     let expected = "twenty";
+//     assert_eq!(output, expected);
+// }
+
+// #[test]
+// #[ignore]
+// fn twenty_two() {
+//     let input = 22;
+//     let output = encode(input);
+//     let expected = "twenty-two";
+//     assert_eq!(output, expected);
+// }
+
+// #[test]
+// #[ignore]
+// fn thirty() {
+//     let input = 30;
+//     let output = encode(input);
+//     let expected = "thirty";
+//     assert_eq!(output, expected);
+// }
+
+// #[test]
+// #[ignore]
+// fn ninety_nine() {
+//     let input = 99;
+//     let output = encode(input);
+//     let expected = "ninety-nine";
+//     assert_eq!(output, expected);
+// }
+
+// #[test]
+// #[ignore]
+// fn one_hundred() {
+//     let input = 100;
+//     let output = encode(input);
+//     let expected = "one hundred";
+//     assert_eq!(output, expected);
+// }
+
+// #[test]
+// #[ignore]
+// fn one_hundred_twenty_three() {
+//     let input = 123;
+//     let output = encode(input);
+//     let expected = "one hundred twenty-three";
+//     assert_eq!(output, expected);
+// }
+
+// #[test]
+// #[ignore]
+// fn two_hundred() {
+//     let input = 200;
+//     let output = encode(input);
+//     let expected = "two hundred";
+//     assert_eq!(output, expected);
+// }
+
+// #[test]
+// #[ignore]
+// fn nine_hundred_ninety_nine() {
+//     let input = 999;
+//     let output = encode(input);
+//     let expected = "nine hundred ninety-nine";
+//     assert_eq!(output, expected);
+// }
+
+// #[test]
+// #[ignore]
+// fn one_thousand() {
+//     let input = 1_000;
+//     let output = encode(input);
+//     let expected = "one thousand";
+//     assert_eq!(output, expected);
+// }
+
+// #[test]
+// #[ignore]
+// fn one_thousand_two_hundred_thirty_four() {
+//     let input = 1_234;
+//     let output = encode(input);
+//     let expected = "one thousand two hundred thirty-four";
+//     assert_eq!(output, expected);
+// }
+
+// #[test]
+// #[ignore]
+// fn one_million() {
+//     let input = 1_000_000;
+//     let output = encode(input);
+//     let expected = "one million";
+//     assert_eq!(output, expected);
+// }
+
+// #[test]
+// #[ignore]
+// fn one_million_two_thousand_three_hundred_forty_five() {
+//     let input = 1_002_345;
+//     let output = encode(input);
+//     let expected = "one million two thousand three hundred forty-five";
+//     assert_eq!(output, expected);
+// }
+
+// #[test]
+// //#[ignore]
+// fn one_billion() {
+//     let input = 1_000_000_000;
+//     let output = encode(input);
+//     let expected = "one billion";
+//     assert_eq!(output, expected);
+// }
+
+// #[test]
+// #[ignore]
+// fn a_big_number() {
+//     let input = 987_654_321_123;
+//     let output = encode(input);
+//     let expected = "nine hundred eighty-seven billion six hundred fifty-four million three hundred twenty-one thousand one hundred twenty-three";
+//     assert_eq!(output, expected);
+// }
+
+// #[test]
+// #[ignore]
+// fn max_i64() {
+//     let input = 9_223_372_036_854_775_807;
+//     let output = encode(input);
+//     let expected = "nine quintillion two hundred twenty-three quadrillion three hundred seventy-two trillion thirty-six billion eight hundred fifty-four million seven hundred seventy-five thousand eight hundred seven";
+//     assert_eq!(output, expected);
+// }
+
+// #[test]
+// #[ignore]
+// fn max_u64() {
+//     let input = 18_446_744_073_709_551_615;
+//     let output = encode(input);
+//     let expected = "eighteen quintillion four hundred forty-six quadrillion seven hundred forty-four trillion seventy-three billion seven hundred nine million five hundred fifty-one thousand six hundred fifteen";
+//     assert_eq!(output, expected);
+// }
