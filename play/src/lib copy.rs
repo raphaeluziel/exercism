3fccb70db17006f5dc9b556cf2f5d8409c614f75
raphaeluziel <raphaeluziel@gmail.com>
2025-06-17 18:27:46 -0400

    getting there

--- play/src/lib copy.rs
+++ play/src/lib copy.rs
@@ -1,11 +1,10 @@
 pub struct SimpleLinkedList<T> {
-    data: Vec<T>,
-    index: usize
+    data: Vec<(T, *const T)>,
 }
 
 impl<T> SimpleLinkedList<T> {
     pub fn new() -> Self {
-        Self { data: Vec::new(), index: 0 }
+        Self { data: Vec::new() }
     }
 
     // You may be wondering why it's necessary to have is_empty()
@@ -22,28 +21,26 @@ impl<T> SimpleLinkedList<T> {
     }
 
     pub fn push(&mut self, _element: T) {
-        self.data.push(_element);
-        self.index += 1;
+        let next = if self.is_empty() {
+            std::ptr::null()
+        } else {
+            std::ptr::from_ref(&self.data.last().unwrap().0)
+        };
+        self.data.push((_element, next));
     }
 
     pub fn pop(&mut self) -> Option<T> {
-        if self.data.is_empty() { None }
-        else {
-            self.index -= 1;
-            self.data.pop()
-        }
+        if self.is_empty() { return None; }
+        Some(self.data.pop().unwrap().0)
     }
 
     pub fn peek(&self) -> Option<&T> {
-        if self.data.is_empty() { None }
-        else { Some(&self.data[self.index - 1]) }
+        todo!()
     }
 
     #[must_use]
     pub fn rev(self) -> SimpleLinkedList<T> {
-        let mut reversed_list = self.data;
-        reversed_list.reverse();
-        SimpleLinkedList { data: reversed_list, index: self.index }
+        todo!()
     }
 }
 
