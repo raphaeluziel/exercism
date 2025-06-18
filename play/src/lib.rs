b412d01bbc16cdd526a48f861cedfc80fe24cc1e
raphaeluziel <raphaeluziel@gmail.com>
2025-06-18 11:14:58 -0400

    leaving

--- play/src/lib.rs
+++ play/src/lib.rs
@@ -8,11 +8,6 @@ impl<T> SimpleLinkedList<T> {
         Self { data: Vec::new(), index: 0 }
     }
 
-    // You may be wondering why it's necessary to have is_empty()
-    // when it can easily be determined from len().
-    // It's good custom to have both because len() can be expensive for some types,
-    // whereas is_empty() is almost always cheap.
-    // (Also ask yourself whether len() is expensive for SimpleLinkedList)
     pub fn is_empty(&self) -> bool {
         self.data.is_empty()
     }
@@ -49,20 +44,14 @@ impl<T> SimpleLinkedList<T> {
 
 impl<T> FromIterator<T> for SimpleLinkedList<T> {
     fn from_iter<I: IntoIterator<Item = T>>(_iter: I) -> Self {
-        todo!()
+        let mut sll = SimpleLinkedList::new();
+        for i in _iter {
+            sll.data.push(i);
+            sll.index += 1;
+        }
+        sll
     }
 }
-
-// In general, it would be preferable to implement IntoIterator for SimpleLinkedList<T>
-// instead of implementing an explicit conversion to a vector. This is because, together,
-// FromIterator and IntoIterator enable conversion between arbitrary collections.
-//
-// The reason this exercise's API includes an explicit conversion to Vec<T> instead
-// of IntoIterator is that implementing that interface is fairly complicated, and
-// demands more of the student than we expect at this point in the track.
-//
-// Please note that the "front" of the linked list should correspond to the "back"
-// of the vector as far as the tests are concerned.
 
 impl<T> From<SimpleLinkedList<T>> for Vec<T> {
     fn from(mut _linked_list: SimpleLinkedList<T>) -> Vec<T> {
