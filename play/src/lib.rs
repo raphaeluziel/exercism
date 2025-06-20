91a299191943d6b1e134651a36036cfb37e59070
Raphael Uziel <raphaeluziel@hotmail.com>
2025-06-20 17:03:11 -0400

    unsafe

--- play/src/lib.rs
+++ play/src/lib.rs
@@ -1,11 +1,10 @@
 pub struct SimpleLinkedList<T> {
-    data: Vec<T>,
-    index: usize
+    data: Vec<(T, *const T)>,
 }
 
 impl<T> SimpleLinkedList<T> {
     pub fn new() -> Self {
-        Self { data: Vec::new(), index: 0 }
+        Self { data: Vec::new() }
     }
 
     pub fn is_empty(&self) -> bool {
@@ -17,44 +16,49 @@ impl<T> SimpleLinkedList<T> {
     }
 
     pub fn push(&mut self, _element: T) {
-        self.data.push(_element);
-        self.index += 1;
+        if let Some(last) = self.data.last_mut() {
+            unsafe {
+                *last.1 = std::ptr::null() as *const T;   
+            }
+        }
+        self.data.push((_element, std::ptr::null()));
     }
 
     pub fn pop(&mut self) -> Option<T> {
-        if self.data.is_empty() { None }
-        else {
-            self.index -= 1;
-            self.data.pop()
-        }
+        let ccc = self.data.pop();
+        todo!()
     }
 
     pub fn peek(&self) -> Option<&T> {
-        if self.data.is_empty() { None }
-        else { Some(&self.data[self.index - 1]) }
+        // if self.data.is_empty() { None }
+        // else { Some(&self.data[self.index - 1]) }
+        None
     }
 
     #[must_use]
     pub fn rev(self) -> SimpleLinkedList<T> {
-        let mut reversed_list = self.data;
-        reversed_list.reverse();
-        SimpleLinkedList { data: reversed_list, index: self.index }
+        // let mut reversed_list = self.data;
+        // reversed_list.reverse();
+        // SimpleLinkedList { data: reversed_list, index: self.index }
+        todo!()
     }
 }
 
 impl<T> FromIterator<T> for SimpleLinkedList<T> {
     fn from_iter<I: IntoIterator<Item = T>>(_iter: I) -> Self {
-        let mut sll = SimpleLinkedList::new();
-        for i in _iter {
-            sll.data.push(i);
-            sll.index += 1;
-        }
-        sll
+        // let mut sll = SimpleLinkedList::new();
+        // for i in _iter {
+        //     sll.data.push(i);
+        //     sll.index += 1;
+        // }
+        // sll
+        todo!()
     }
 }
 
 impl<T> From<SimpleLinkedList<T>> for Vec<T> {
     fn from(mut _linked_list: SimpleLinkedList<T>) -> Vec<T> {
-        _linked_list.data
+        // _linked_list.data
+        todo!()
     }
 }
