e3b018aaad31a4e88bbc3e039c360d14722c82d7
raphaeluziel <raphaeluziel@gmail.com>
2025-06-28 09:45:50 -0400

    finished linked list

--- play/tests/play.rs
+++ play/tests/play.rs
@@ -1,122 +1,65 @@
 use play::*;
 
 #[test]
-//#[ignore]
-fn new_list_is_empty() {
-    let list: SimpleLinkedList<u32> = SimpleLinkedList::new();
-    assert_eq!(list.len(), 0, "list's length must be 0");
-}
-
-#[test]
-//#[ignore]
-fn push_increments_length() {
-    let mut list: SimpleLinkedList<u32> = SimpleLinkedList::new();
-    list.push(1);
-    assert_eq!(list.len(), 1, "list's length must be 1");
-    list.push(2);
-    assert_eq!(list.len(), 2, "list's length must be 2");
-}
-
-#[test]
-//#[ignore]
-fn pop_decrements_length() {
-    let mut list: SimpleLinkedList<u32> = SimpleLinkedList::new();
-    list.push(1);
-    list.push(2);
-    list.pop();
-    assert_eq!(list.len(), 1, "list's length must be 1");
-    list.pop();
-    assert_eq!(list.len(), 0, "list's length must be 0");
-}
-
-#[test]
-//#[ignore]
-fn is_empty() {
-    let mut list: SimpleLinkedList<u32> = SimpleLinkedList::new();
-    assert!(list.is_empty(), "List wasn't empty on creation");
-    for inserts in 0..100 {
-        for i in 0..inserts {
-            list.push(i);
-            assert!(
-                !list.is_empty(),
-                "List was empty after having inserted {i}/{inserts} elements"
-            );
-        }
-        for i in 0..inserts {
-            assert!(
-                !list.is_empty(),
-                "List was empty before removing {i}/{inserts} elements"
-            );
-            list.pop();
-        }
-        assert!(
-            list.is_empty(),
-            "List wasn't empty after having removed {inserts} elements"
-        );
-    }
-}
-
-#[test]
-//#[ignore]
-fn pop_returns_head_element_and_removes_it() {
-    let mut list: SimpleLinkedList<u32> = SimpleLinkedList::new();
-    list.push(1);
-    list.push(2);
-    assert_eq!(list.pop(), Some(2), "Element must be 2");
-    assert_eq!(list.pop(), Some(1), "Element must be 1");
-    assert_eq!(list.pop(), None, "No element should be contained in list");
-}
-
-#[test]
-//#[ignore]
-fn peek_returns_reference_to_head_element_but_does_not_remove_it() {
-    let mut list: SimpleLinkedList<u32> = SimpleLinkedList::new();
-    assert_eq!(list.peek(), None, "No element should be contained in list");
-    list.push(2);
-    assert_eq!(list.peek(), Some(&2), "Element must be 2");
-    assert_eq!(list.peek(), Some(&2), "Element must be still 2");
-    list.push(3);
-    assert_eq!(list.peek(), Some(&3), "Head element is now 3");
-    assert_eq!(list.pop(), Some(3), "Element must be 3");
-    assert_eq!(list.peek(), Some(&2), "Head element is now 2");
-    assert_eq!(list.pop(), Some(2), "Element must be 2");
-    assert_eq!(list.peek(), None, "No element should be contained in list");
-}
-
-#[test]
-//#[ignore]
-fn from_slice() {
-    let mut array = vec!["1", "2", "3", "4"];
-    let mut list: SimpleLinkedList<_> = array.drain(..).collect();
-    assert_eq!(list.pop(), Some("4"));
-    assert_eq!(list.pop(), Some("3"));
-    assert_eq!(list.pop(), Some("2"));
-    assert_eq!(list.pop(), Some("1"));
+fn empty_spiral() {
+    let input = 0;
+    let output = spiral_matrix(input);
+    let expected: [[u32; 0]; 0] = [];
+    assert_eq!(output, expected);
 }
 
 #[test]
 #[ignore]
-fn reverse() {
-    let mut list: SimpleLinkedList<u32> = SimpleLinkedList::new();
-    list.push(1);
-    list.push(2);
-    list.push(3);
-    let mut rev_list = list.rev();
-    assert_eq!(rev_list.pop(), Some(1));
-    assert_eq!(rev_list.pop(), Some(2));
-    assert_eq!(rev_list.pop(), Some(3));
-    assert_eq!(rev_list.pop(), None);
+fn trivial_spiral() {
+    let input = 1;
+    let output = spiral_matrix(input);
+    let expected: [[u32; 1]; 1] = [[1]];
+    assert_eq!(output, expected);
 }
 
 #[test]
-//#[ignore]
-fn into_vector() {
-    let mut v = Vec::new();
-    let mut s = SimpleLinkedList::new();
-    for i in 1..4 {
-        v.push(i);
-        s.push(i);
-    }
-    let s_as_vec: Vec<i32> = s.into();
-    assert_eq!(v, s_as_vec);
+#[ignore]
+fn spiral_of_size_2() {
+    let input = 2;
+    let output = spiral_matrix(input);
+    let expected: [[u32; 2]; 2] = [[1, 2], [4, 3]];
+    assert_eq!(output, expected);
+}
+
+#[test]
+#[ignore]
+fn spiral_of_size_3() {
+    let input = 3;
+    let output = spiral_matrix(input);
+    let expected: [[u32; 3]; 3] = [[1, 2, 3], [8, 9, 4], [7, 6, 5]];
+    assert_eq!(output, expected);
+}
+
+#[test]
+#[ignore]
+fn spiral_of_size_4() {
+    let input = 4;
+    let output = spiral_matrix(input);
+    let expected: [[u32; 4]; 4] = [
+        [1, 2, 3, 4],
+        [12, 13, 14, 5],
+        [11, 16, 15, 6],
+        [10, 9, 8, 7],
+    ];
+    assert_eq!(output, expected);
+}
+
+#[test]
+#[ignore]
+fn spiral_of_size_5() {
+    let input = 5;
+    let output = spiral_matrix(input);
+    let expected: [[u32; 5]; 5] = [
+        [1, 2, 3, 4, 5],
+        [16, 17, 18, 19, 6],
+        [15, 24, 25, 20, 7],
+        [14, 23, 22, 21, 8],
+        [13, 12, 11, 10, 9],
+    ];
+    assert_eq!(output, expected);
 }
