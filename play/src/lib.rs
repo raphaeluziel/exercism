98a8eca437b00420a9564be044008677b5a7eebb
raphaeluziel <raphaeluziel@gmail.com>
2025-07-14 11:19:21 -0400

    getting there

--- play/src/lib.rs
+++ play/src/lib.rs
@@ -1,43 +1,54 @@
 pub fn spiral_matrix(size: u32) -> Vec<Vec<u32>> {
+    let size = 7;
+
     let mut v = vec![vec![0; size as usize]; size as usize];
 
     if size == 0 { return v; }
     if size == 1 { return vec![vec![1]]; }
 
+    let last = size * size;
+    let mut count = 1u32;
+    let mut start = 0;
+    let mut end = size as usize;
+    let mut i = 0usize;
+    let mut j = 0usize;
 
-
-    
-
-
-
-
-
-
-
-    let mut n = 1i32;
-    let mut i = 0i32;
-    let mut j = 0i32;
-    let max: i32 = (size * size) as i32;
-
-    let mut x = 0;
-    let a: [(i32, i32); 4] = [(1, 0), (0, 1), (-1, 0), (0, -1)];
-
-    let mut m = (size - 1) as i32; 
-
-    println!("size = {size}");
-    while n < max {
-        //println!("i = {i}, j = {j}");
-        //println!("x = {x}");
-        v[i as usize][j as usize] = n as u32;
-        n += 1;
-
-        i += a[x].0;
-        j += a[x].1;
-        
-        if (i == m && j < m) || (j == m && i < m) {
-            x = if x == 3 { 0 } else { x + 1 };
-            m -= 1;
+    while count < last {
+        println!(">>>>>>>");
+        while i < end {
+            println!("i = {i}, j = {j}, count = {count}");
+            v[i][j] = count;
+            count += 1;
+            i += 1;
         }
+        j += 1;
+        i -= 1;
+        println!("VVVVVVV");
+        while j < end {
+            println!("i = {i}, j = {j}, count = {count}");
+            v[i][j] = count;
+            count += 1;
+            j += 1;
+        }
+        j -= 2;
+        i -= 1;
+        println!("<<<<<<<");
+        while i > start {
+            println!("i = {i}, j = {j}, count = {count}");
+            v[i][j] = count;
+            count += 1;
+            i -= 1;
+        }
+        j -= 1;
+        i += 1;
+        println!("AAAAAAA");
+        while j > start {
+            println!("i = {i}, j = {j}, count = {count}");
+            v[i][j] = count;
+            count += 1;
+            j -= 1;
+        }
+        i -= 1;
     }
 
     v
