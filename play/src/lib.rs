1b36641e3dc7a6e61b33fbf6e18b4c50e2065626
raphaeluziel <raphaeluziel@gmail.com>
2025-07-29 11:44:51 -0400

    new approach?

--- play/src/lib.rs
+++ play/src/lib.rs
@@ -1,57 +1,34 @@
+fn move_right(ser: (usize, usize)) -> (usize, usize, bool) {
+    (ser.0, ser.1, false)
+}
+
+fn move_down(ser: (usize, usize)) -> (usize, usize, bool) {
+    (ser.0 + 1, ser.1, false)
+}
+
+fn move_left(ser: (usize, usize)) -> (usize, usize, bool) {
+    (ser.0 - 1, ser.1 - 1, true)
+}
+
+fn move_up(ser: (usize, usize)) -> (usize, usize, bool) {
+    (ser.0 + 1, ser.1, true)
+}
+
 pub fn spiral_matrix(size: u32) -> Vec<Vec<u32>> {
     let size = 7;
 
-    let mut v = vec![vec![0isize; size as usize]; size as usize];
+    let mut v = vec![vec![0u32; size as usize]; size as usize];
 
-    // if size == 0 { return v; }
-    // if size == 1 { return vec![vec![1]]; }
+    if size == 0 { return v; }
+    if size == 1 { return vec![vec![1]]; }
 
-    let last = size * size as isize;
-    let mut count = 1isize;
-    let mut start = 0isize;
-    let mut end = size as isize;
-    let mut i = 0isize;
-    let mut j = 0isize;
+    let last = size * size;
+    let mut count = 1usize;
+    let mut start = 0usize;
+    let mut end = size;
 
     while count < last {
-        println!(">>>>>>>");
-        while i < end {
-            println!("i = {i}, j = {j}, count = {count}");
-            v[i as usize][j as usize] = count;
-            count += 1;
-            i += 1;
-        }
-        j += 1;
-        i -= 1;
-        println!("VVVVVVV");
-        while j < end {
-            println!("i = {i}, j = {j}, count = {count}");
-            v[i as usize][j as usize] = count;
-            count += 1;
-            j += 1;
-        }
-        j -= 1;
-        i -= 1;
-        println!("<<<<<<<");
-        while i >= start {
-            println!("i = {i}, j = {j}, count = {count}");
-            v[i as usize][j as usize] = count;
-            count += 1;
-            i -= 1;
-        }
-        j -= 1;
-        i += 1;
-        println!("AAAAAAA");
-        while j >= start {
-            println!("i = {i}, j = {j}, count = {count}");
-            v[i as usize][j as usize] = count;
-            count += 1;
-            j -= 1;
-        }
-        i += 1;
-        j += 1;
-
-
+        
     }
 
     todo!()
