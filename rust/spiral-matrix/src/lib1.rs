793376dc0e638ba421ef848cd9861b9e7a1d02b0
Raphael Uziel <raphaeluziel@hotmail.com>
2025-08-01 16:16:04 -0400

    ok

--- rust/spiral-matrix/src/lib1.rs
+++ rust/spiral-matrix/src/lib1.rs
@@ -1,6 +1,4 @@
 pub fn spiral_matrix(size: u32) -> Vec<Vec<u32>> {
-    let size = 4;
-
     let mut v = vec![vec![0u32; size as usize]; size as usize];
 
     if size == 0 {
@@ -16,51 +14,34 @@ pub fn spiral_matrix(size: u32) -> Vec<Vec<u32>> {
     let mut end = size as usize - 1;
 
     while count <= last {
-
         // Move right (do nothing to start or end)
-        println!(">>>>>>>");
         for col in start..=end {
-            println!("{col}, {start} ---> {count}");
             v[start][col] = count;
             count += 1;
         }
 
         // Move down (increase start by 1)
-        println!("VVVVVVV");
         start += 1;
         for row in start..=end {
-            println!("{end}, {row} ---> {count}");
             v[row][end] = count;
             count += 1;
         }
 
         // Move left (decrease start by 1 and decrease end by 1)
-        println!("<<<<<<<");
         start -= 1;
         end -= 1;
         for col in (start..=end).rev() {
-            println!("{col}, {} ---> {count}", end + 1);
             v[end + 1][col] = count;
             count += 1;
         }
 
         // Move up (increase start by 1)
-        println!("AAAAAAA");
         start += 1;
         for row in (start..=end).rev() {
-            println!("{}, {row} ---> {count}", start - 1);
             v[row][start - 1] = count;
             count += 1;
         }
-
     }
-
-    println!("\n{:?}", v);
-
-    let mut hhh = || v[0][0] = 22222222;
-    hhh();
-
-    println!("{:?}", v);
 
     v
 }
--- rust/spiral-matrix/src/lib2.rs
+++ rust/spiral-matrix/src/lib2.rs
@@ -1,8 +1,6 @@
 pub fn spiral_matrix(size: u32) -> Vec<Vec<u32>> {
-    let size = 4;
-
     let mut v = vec![vec![0u32; size as usize]; size as usize];
-
+    
     if size == 0 {
         return v;
     }
@@ -10,57 +8,39 @@ pub fn spiral_matrix(size: u32) -> Vec<Vec<u32>> {
         return vec![vec![1]];
     }
 
-    let last = size * size;
     let mut count = 1u32;
     let mut start = 0usize;
     let mut end = size as usize - 1;
 
-    while count <= last {
-
+    while count <= size * size {
         // Move right (do nothing to start or end)
-        println!(">>>>>>>");
         for col in start..=end {
-            println!("{col}, {start} ---> {count}");
             v[start][col] = count;
             count += 1;
         }
 
         // Move down (increase start by 1)
-        println!("VVVVVVV");
         start += 1;
         for row in start..=end {
-            println!("{end}, {row} ---> {count}");
             v[row][end] = count;
             count += 1;
         }
 
         // Move left (decrease start by 1 and decrease end by 1)
-        println!("<<<<<<<");
         start -= 1;
         end -= 1;
         for col in (start..=end).rev() {
-            println!("{col}, {} ---> {count}", end + 1);
             v[end + 1][col] = count;
             count += 1;
         }
-
+        
         // Move up (increase start by 1)
-        println!("AAAAAAA");
         start += 1;
         for row in (start..=end).rev() {
-            println!("{}, {row} ---> {count}", start - 1);
             v[row][start - 1] = count;
             count += 1;
         }
-
     }
-
-    println!("\n{:?}", v);
-
-    let mut hhh = || v[0][0] = 22222222;
-    hhh();
-
-    println!("{:?}", v);
 
     v
 }
